a69ead9ef867f235fdf2762e9906ca82b1d4586c
Jose <jose.ibarra.dev+git@gmail.com>
2016-12-10 01:00:40 -0700

    Part 2 day 3

--- 2016/day3/day3.js
+++ 2016/day3/day3.js
@@ -4,20 +4,15 @@ var PossibleTriangle = function(){
 	this.lineRegex = new RegExp("\\s+");
 
 	this.run = function(){
-		this.getInput(this.puzzleInput);
-		this.part1(this.input2);
-		// this.part1(this.input3.split('\n'));
-	};
-
-	this.getInput = function(fileName){
-		// client.onreadystatechange = function() {
-		// 	functionToRun(client.responseText.split('\n'));
-		// }
 		var that = this;
-		$.get(fileName, function(data){
+		$.get(that.puzzleInput, function(data){
 			that.part1(data.split('\n'));
 		});
-	}
+
+		$.get(that.puzzleInput, function(data){
+			that.part2(data.split('\n'));
+		});
+	};
 
 	this.part1 = function(input){
 		var possibleTriangleCount = 0;
@@ -25,6 +20,26 @@ var PossibleTriangle = function(){
 			var triangleSides = input[i].split(this.lineRegex);
 			//Remember to parse int. Caused problems earlier doing comparison with the strings
 			if(this.isValidTriangle(parseInt(triangleSides[1]), parseInt(triangleSides[2]), parseInt(triangleSides[3])) === true){
+				possibleTriangleCount++;
+			}
+		}
+		console.log("Possible Triangles: " + possibleTriangleCount);
+	};
+
+	this.part2 = function(input){
+		var possibleTriangleCount = 0;
+		for(var i = 0; i < input.length; i+=3){
+			var triangleSides1 = input[i].split(this.lineRegex);
+			var triangleSides2 = input[i+1].split(this.lineRegex);
+			var triangleSides3 = input[i+2].split(this.lineRegex);
+			//Remember to parse int. Caused problems earlier doing comparison with the strings
+			if(this.isValidTriangle(parseInt(triangleSides1[1]), parseInt(triangleSides2[1]), parseInt(triangleSides3[1])) === true){
+				possibleTriangleCount++;
+			}
+			if(this.isValidTriangle(parseInt(triangleSides1[2]), parseInt(triangleSides2[2]), parseInt(triangleSides3[2])) === true){
+				possibleTriangleCount++;
+			}
+			if(this.isValidTriangle(parseInt(triangleSides1[3]), parseInt(triangleSides2[3]), parseInt(triangleSides3[3])) === true){
 				possibleTriangleCount++;
 			}
 		}
