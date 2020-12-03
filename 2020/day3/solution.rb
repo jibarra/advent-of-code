6494957a7c0dc3899668eef21cc5e444ff53a97c
Jose Ibarra <developer@icepicky.com>
2020-12-03 06:45:52 -0700

    Update solution to be more reusable for part 2

--- 2020/day3/solution.rb
+++ 2020/day3/solution.rb
@@ -5,18 +5,23 @@ lines.each do |line|
   tree_coordinates << line.gsub("\n", '').split('')
 end
 
-bottom_of_mountain = tree_coordinates.size
-current_y = 0
-current_x = 0
-tree_count = 0
+def trees_encountered(tree_coordinates, add_x, add_y)
+  bottom_of_mountain = tree_coordinates.size
+  current_y = 0
+  current_x = 0
+  tree_count = 0
 
-while(current_y < bottom_of_mountain)
-  if tree_coordinates[current_y][current_x] == '#'
-    tree_count = tree_count + 1
+  while(current_y < bottom_of_mountain)
+    if tree_coordinates[current_y][current_x] == '#'
+      tree_count = tree_count + 1
+    end
+
+    current_x = (current_x + add_x) % tree_coordinates[current_y].size
+    current_y = current_y + add_y
   end
 
-  current_x = (current_x + 3) % tree_coordinates[current_y].size
-  current_y = current_y + 1
+  tree_count
 end
 
+tree_count = trees_encountered(tree_coordinates, 3, 1)
 puts tree_count
