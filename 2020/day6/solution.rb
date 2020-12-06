3d70170477417fab0cc822d6491e90668ef9c0d0
Jose Ibarra <developer@icepicky.com>
2020-12-06 06:53:57 -0700

    2020 day 6 part 2

--- 2020/day6/solution.rb
+++ 2020/day6/solution.rb
@@ -22,3 +22,30 @@ end
 
 sum_yes_count += group_answers.size
 puts sum_yes_count
+
+# Part 2
+group_answers = Hash.new(0)
+group_size = 0
+sum_yes_count = 0
+
+lines.each do |line|
+  line = line.strip
+  if line.empty?
+    group_answers.each do |key, value|
+      sum_yes_count += 1 if value == group_size
+    end
+    group_answers = Hash.new(0)
+    group_size = 0
+  else
+    group_size += 1
+    line.split('').each do |answer|
+      group_answers[answer] += 1
+    end
+  end
+end
+
+group_answers.each do |key, value|
+  sum_yes_count += 1 if value == group_size
+end
+
+puts sum_yes_count
