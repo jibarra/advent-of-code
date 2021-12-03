ca22416b117e86e737bbdb90d1b993f2e7b2d6f1
Jose Ibarra <developer@icepicky.com>
2021-12-02 18:44:20 -0700

    refactor: 2021 day 1 with each_cons

--- 2021/day1/solution.rb
+++ 2021/day1/solution.rb
@@ -1,11 +1,7 @@
-# Part 1
-# Could have two loops, one inside the other.
-# Could also put all the numbers into something like a hash, and loop through it once.
-#   Subtract the current number from 2020 and see if that result exists.
-
 lines = File.readlines('./input.txt')
 line_depths = lines.map(&:to_i)
 
+# Part 1
 previous_depth = nil
 measurements_larger_than_last = 0
 line_depths.each do |depth|
@@ -17,16 +13,15 @@ end
 
 puts measurements_larger_than_last
 
+# Part 2
 sliding_windows_larger_than_last = 0
-previous_sliding_window_sum = nil
-i = 0
-while i < line_depths.length - 2
-  current_sliding_window_sum = line_depths[i] + line_depths[i + 1] + line_depths[i + 2]
-  if previous_sliding_window_sum && previous_sliding_window_sum < current_sliding_window_sum
+line_depths.each_cons(4) do |measurements|
+  first_sliding_window = measurements[0] + measurements[1] + measurements[2]
+  second_sliding_window = measurements[1] + measurements[2] + measurements[3]
+
+  if first_sliding_window < second_sliding_window
     sliding_windows_larger_than_last += 1
   end
-  previous_sliding_window_sum = current_sliding_window_sum
-  i += 1
 end
 
 puts sliding_windows_larger_than_last
