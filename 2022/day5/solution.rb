0c67c7f6be960cdf2e6f5b39a5e962f5758094cd
Jose Ibarra <developer@icepicky.com>
2022-12-06 08:19:47 -0700

    2022 day 5 part 1

---
+++ 2022/day5/solution.rb
@@ -0,0 +1,44 @@
+lines = File.readlines('./input.txt')
+
+separating_line = lines.find_index { |line| line.strip.empty? }
+
+stack_count = lines[separating_line - 1].split(' ').last.to_i
+
+stacks = (0..stack_count - 1).map do |stack_index|
+  []
+end
+
+(0..(separating_line - 2)).each do |line_number|
+  line_index = 1
+  (0..(stack_count - 1)).each do |stack_index|
+    if !lines[line_number][line_index].strip.empty?
+      stacks[stack_index].unshift(lines[line_number][line_index])
+    end
+    line_index += 4
+  end
+end
+
+
+instructions = lines[separating_line + 1..-1].map(&:strip)
+
+instructions.each do |instruction|
+  stack_instructions = instruction.scan(/\d+/).map(&:to_i)
+  quantity_to_move = stack_instructions[0]
+  stack_to_move_from = stacks[stack_instructions[1] - 1]
+  stack_to_move_to = stacks[stack_instructions[2] - 1]
+
+  (0..quantity_to_move - 1).each do |_|
+    item_to_move = stack_to_move_from.pop
+    if item_to_move
+      stack_to_move_to.push(item_to_move)
+    end
+  end
+end
+
+top_of_stack = ''
+stacks.each do |stack|
+  top_of_stack = top_of_stack + (stack.last || '')
+end
+
+# Part 1
+top_of_stack
