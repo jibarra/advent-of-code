c4340a07cd7394df7b3640f531e44dbc592ff052
Jose Ibarra <developer@icepicky.com>
2022-12-10 07:38:16 -0700

    2022 day 10 part 2

--- 2022/day10/solution.rb
+++ 2022/day10/solution.rb
@@ -36,3 +36,61 @@ lines.each do |line|
 end
 
 puts signal_strengths.compact.sum
+
+LIT = '#'
+DARK = '.'
+
+def draw_screen(sprite_position, crt_position)
+  if (sprite_position-1..sprite_position+1).include?(crt_position)
+    LIT
+  else
+    DARK
+  end
+end
+
+def add_row_to_screen(crt_screen)
+  current_row = crt_screen.last
+  if !current_row || current_row.none? { |c| c.nil? }
+    row = (1..40).map { |_| nil }
+    crt_screen.push(row)
+  end
+end
+
+screen = []
+add_row_to_screen(screen)
+
+current_sprite_position = 1
+current_crt_position = 0
+
+lines.each do |line|
+  instruction, potential_number = line.split(' ')
+
+  if instruction == NOOP
+    # one cycle
+    add_row_to_screen(screen)
+    screen.last[current_crt_position] = draw_screen(current_sprite_position, current_crt_position)
+    current_crt_position = (current_crt_position + 1) % 40
+  elsif instruction == ADD
+    # first cycle
+    add_row_to_screen(screen)
+    screen.last[current_crt_position] = draw_screen(current_sprite_position, current_crt_position)
+    current_crt_position = (current_crt_position + 1) % 40
+
+    # second cycle
+    add_row_to_screen(screen)
+    screen.last[current_crt_position] = draw_screen(current_sprite_position, current_crt_position)
+    current_crt_position = (current_crt_position + 1) % 40
+
+    current_sprite_position += potential_number.to_i
+  else
+    raise ArgumentError, "invalid instruction: #{instruction}"
+  end
+end
+
+screen.each do |screen_line|
+  string_line = ''
+  screen_line.each do |c|
+    string_line += c
+  end
+  puts string_line
+end
